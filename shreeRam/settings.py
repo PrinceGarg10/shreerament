803a18ce002dab9fee066b55750f5d78360c1c43
raj-2407 <krishanagarg879@gmail.com>
2021-10-08 11:47:26 +0530

    some changes in settings

--- shreeRam/settings.py
+++ shreeRam/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #added manually
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
+    os.path.join(BASE_DIR / "static",
+    '/var/www/static/',)
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
